a46d4c34639fb3a149c3eff8a9de6a6b8b8e8732
Avery Durrant <avery246813579@gmail.com>
2017-03-03 10:45:41 -0500

    Improved the code for easier blog deployment and fixed image overhang.

--- assets/scripts/blogHandler.js
+++ assets/scripts/blogHandler.js
@@ -26,7 +26,6 @@ if (ref.indexOf('?') != -1) {
     ref = ref.substr(0, ref.indexOf('?'));
 }
 
-var blogsFiles = ['10 4 16 BLOG.html', '10 16 16 BLOG.html', '11 11 16 BLOG.html', '12 19 16 BLOG.html', '1 6 17 BLOG.html'];
 var blogsHtml = {};
 var index = blogsFiles.length - 1;
 
